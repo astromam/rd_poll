b7020a04eff208074cf97657c1b1ffd3711687df
Mamadou <mamadou.ndiaye@oca.eu>
2024-04-04 18:47:56 +0200

    modify the code to make wordcloud for different dataset of the rd poll

--- scripts/poll_data_analysis.py
+++ scripts/poll_data_analysis.py
@@ -20,17 +20,20 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 #%%
 """
-### Directories and paths
+### Directories and paths to read files
 """
 fdir = Path('/Users/mndiaye/python/rd_poll/data/').resolve()
 
-fname_proj = 'results-survey149427_projets_2024-03-21_v2.xlsx'
-fname_labo = 'results-survey966749_labo_2024-03-21.xlsx'
+fname_proj = 'results-survey149427_projets_2024-03-25_cleaned.xlsx'
+fname_labo = 'results-survey966749_labo_2024-03-25_cleaned.xlsx'
 
 fpath_proj = fdir / fname_proj
 fpath_labo = fdir / fname_labo
 
 #%%
+"""
+### Directories and paths to save files
+"""
 fdir_sav = Path('/Users/mndiaye/python/rd_poll/plots/').resolve()
 if not os.path.exists(fdir_sav):
     os.makedirs(fdir_sav)
@@ -50,10 +53,11 @@ data_proj_raw = pd.read_excel(fpath_proj, header=0, names=col_name) # dimensions
 ### Select the rows corresponding to the files of the people who went to the final page 
 """
 #s2 = data_proj_raw['Dernière page']
-s2 = data_proj_raw['2']
-idx_finalpage = s2 == 8.0
+# s2 = data_proj_raw['2']
+# idx_finalpage = s2 == 8.0
 
-data_proj = data_proj_raw.loc[idx_finalpage,:]
+# data_proj = data_proj_raw.loc[idx_finalpage,:]
+data_proj = data_proj_raw.loc[:,:]
 
 #%%
 """
@@ -63,7 +67,9 @@ labos_lst = data_proj['5']
 
 uniquelabos_lst = labos_lst.unique()
 
-test  = labos_lst.value_counts()
+labos_stats  = labos_lst.value_counts()
+
+print(labos_stats)
 
 #%%
 """
@@ -73,7 +79,9 @@ personnel_lst = data_proj['7']
 
 uniquepersonnel_lst = personnel_lst.unique()
 
-test2  = personnel_lst.value_counts()
+personnel_stats  = personnel_lst.value_counts()
+
+print(personnel_stats)
 
 #%%
 """
@@ -83,7 +91,9 @@ gender_lst = data_proj['9']
 
 uniquegender_lst = gender_lst.unique()
 
-gender_test = gender_lst.value_counts()
+gender_stats = gender_lst.value_counts()
+
+print(gender_stats)
 
 #%%
 """
@@ -93,7 +103,9 @@ age_lst = data_proj['10']
 
 uniqueage_lst = age_lst.unique()
 
-age_test = age_lst.value_counts()
+age_stats = age_lst.value_counts()
+
+print(age_stats)
 
 #%%
 """
@@ -101,9 +113,11 @@ age_test = age_lst.value_counts()
 """
 training_lst = data_proj['11']
 
-uniquetraining_lst = age_lst.unique()
+uniquetraining_lst = training_lst.unique()
 
-training_test = training_lst.value_counts()
+training_stats = training_lst.value_counts()
+
+print(training_stats)
 
 #%%
 """
@@ -113,7 +127,9 @@ title_lst = data_proj['6']
 
 uniquetitle_lst = title_lst.unique()
 
-title_test  = title_lst.value_counts()
+title_stats  = title_lst.value_counts()
+
+print(title_stats)
 
 #%%
 """
@@ -124,15 +140,15 @@ text = title_lst.tolist()
 ### join the words from all the input
 collapsed_text = (' ').join(text)
 ### remove useless words in French
-collapsed_text2 = collapsed_text.replace(' de ', ' ').replace(' le ', ' ').replace(' pour ', ' ').replace(' du ', ' ').replace(' la ', ' ').replace(' à ', ' ').replace(' un ', ' ').replace(' une ', ' ').replace(' et ', ' ').replace(' des ', ' ')
+collapsed_text_bis = collapsed_text.replace(' de ', ' ').replace(' le ', ' ').replace(' pour ', ' ').replace(' du ', ' ').replace(' la ', ' ').replace(' à ', ' ').replace(' un ', ' ').replace(' une ', ' ').replace(' et ', ' ').replace(' des ', ' ').replace(' en ', ' ').replace(' dans ', ' ')
 ### remove useless words in English
-collapsed_text3 = collapsed_text2.replace(' of ', ' ').replace(' the ', ' ').replace(' for ', ' ').replace(' in ', ' ').replace(' at ', ' ')
+collapsed_text_ter = collapsed_text_bis.replace(' of ', ' ').replace(' the ', ' ').replace(' for ', ' ').replace(' in ', ' ').replace(' at ', ' ')
 
 #%%
 """
 ### Use WordCloud class to generate the data to plot
 """
-wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(collapsed_text3)
+title_wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(collapsed_text_ter)
 
 #%%
 """
@@ -143,9 +159,115 @@ fpath_plt = fdir_sav / fname_plt
 
 plt.figure(0)
 plt.clf()
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.imshow(title_wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.title('Word cloud for project titles')
 plt.tight_layout()
 plt.savefig(fpath_plt)
 plt.show()
+
+#%%
+"""
+### Question scientifique qui motive le projet
+"""
+sci_question_lst = data_proj['27']
+
+uniquesci_question_lst = sci_question_lst.unique()
+
+sci_question_stats  = sci_question_lst.value_counts()
+
+print(sci_question_stats)
+
+#%%
+"""
+### Process the science question data
+"""
+### convert title_lst into a list
+text27 = sci_question_lst.tolist()
+### join the words from all the input
+collapsed_text27 = (' ').join(str(e) for e in text27)
+### remove useless words in French
+collapsed_text27_bis = collapsed_text27.replace(' le ', ' ').replace(' la ', ' ').replace(' les ', ' ').replace(' par ', ' ').replace(' pour ', ' ').replace(' sur ', ' ').replace(' d\'un ', ' ').replace(' d\'une ', ' ').replace(' d ', ' ').replace(' de ', ' ').replace(' du ', ' ').replace(' à ', ' ').replace(' aux ', ' ').replace(' ou ', ' ').replace(' un ', ' ').replace(' une ', ' ').replace(' et ', ' ').replace(' des ', ' ').replace(' en ', ' ').replace(' dans ', ' ')
+### remove useless words in English
+collapsed_text27_ter = collapsed_text27_bis.replace(' of ', ' ').replace(' the ', ' ').replace(' for ', ' ').replace(' in ', ' ').replace(' at ', ' ').replace(' nan ', ' ').replace('nan ', ' ')
+
+#%%
+"""
+### Use WordCloud class to generate the data to plot
+"""
+sci_question_wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(collapsed_text27_ter)
+
+#%%
+"""
+### plot of wordcloud for project titles
+"""
+fname_plt = 'wordcloud_sci_question.png'
+fpath_plt = fdir_sav / fname_plt
+
+plt.figure(1)
+plt.clf()
+plt.imshow(sci_question_wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title('Word cloud for science questions')
+plt.tight_layout()
+plt.savefig(fpath_plt)
+plt.show()
+
+#%%
+"""
+### Mots-clé
+"""
+sci_keywords_lst = data_proj['32']
+
+uniquesci_keywords_lst = sci_keywords_lst.unique()
+
+sci_keywords_stats  = sci_keywords_lst.value_counts()
+
+print(sci_keywords_stats)
+
+#%%
+"""
+### Process the science keywords data
+"""
+### convert title_lst into a list
+text32 = sci_keywords_lst.tolist()
+### join the words from all the input
+collapsed_text32 = (' ').join(str(e) for e in text32)
+### remove useless words in French
+collapsed_text32_bis = collapsed_text32.replace(' le ', ' ').replace(' la ', ' ').replace(' les ', ' ').replace(' par ', ' ').replace(' pour ', ' ').replace(' sur ', ' ').replace(' d\'un ', ' ').replace(' d\'une ', ' ').replace(' d ', ' ').replace(' de ', ' ').replace(' du ', ' ').replace(' à ', ' ').replace(' aux ', ' ').replace(' ou ', ' ').replace(' un ', ' ').replace(' une ', ' ').replace(' et ', ' ').replace(' des ', ' ').replace(' en ', ' ').replace(' dans ', ' ')
+### remove useless words in English
+collapsed_text32_ter = collapsed_text32_bis.replace(' of ', ' ').replace(' the ', ' ').replace(' for ', ' ').replace(' in ', ' ').replace(' at ', ' ').replace(' nan ', ' ').replace('nan ', ' ')
+
+#%%
+"""
+### Use WordCloud class to generate the data to plot
+"""
+sci_keywords_wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(collapsed_text32_ter)
+
+#%%
+"""
+### plot of wordcloud for project titles
+"""
+fname_plt = 'wordcloud_sci_keywords.png'
+fpath_plt = fdir_sav / fname_plt
+
+plt.figure(2)
+plt.clf()
+plt.imshow(sci_keywords_wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title('Word cloud for science keywords')
+plt.tight_layout()
+plt.savefig(fpath_plt)
+plt.show()
+
+#%%
+"""
+### Durée projet
+"""
+project_duration_lst = data_proj['46']
+
+uniqueproject_duration_lst = project_duration_lst.unique()
+
+project_duration_stats = project_duration_lst.value_counts()
+
+print(project_duration_stats)
